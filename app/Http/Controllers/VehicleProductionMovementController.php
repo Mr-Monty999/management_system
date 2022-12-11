2f580dd7eb80a5c5c2c4bf5f31af6392952fde67
Mr-Monty999 <montser.mohmed2022@gmail.com>
2022-12-11 21:14:13 +0200

    Add Vehicle Production Movement

---
+++ app/Http/Controllers/VehicleProductionMovementController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVehicleProductionMovementRequest;
+use App\Http\Requests\UpdateVehicleProductionMovementRequest;
+use App\Models\VehicleProductionMovement;
+
+class VehicleProductionMovementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreVehicleProductionMovementRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreVehicleProductionMovementRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\VehicleProductionMovement  $vehicleProductionMovement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(VehicleProductionMovement $vehicleProductionMovement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\VehicleProductionMovement  $vehicleProductionMovement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(VehicleProductionMovement $vehicleProductionMovement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateVehicleProductionMovementRequest  $request
+     * @param  \App\Models\VehicleProductionMovement  $vehicleProductionMovement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateVehicleProductionMovementRequest $request, VehicleProductionMovement $vehicleProductionMovement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\VehicleProductionMovement  $vehicleProductionMovement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(VehicleProductionMovement $vehicleProductionMovement)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_12_11_191101_create_vehicle_production_movements_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_production_movements', function (Blueprint $table) {
+            $table->id();
+            $table->string("driver");
+            $table->string("type");
+            $table->dateTime("date");
+            $table->text("note")->nullable();
+            $table->unsignedBigInteger("vehicle_id");
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign("vehicle_id")->references("id")->on("vehicles")->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_production_movements');
+    }
+};
