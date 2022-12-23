1e0de94e6422053ce6effc31ac56362132124a88
Mr-Monty999 <montser.mohmed2022@gmail.com>
2022-12-23 11:32:04 +0200

    Update Person Borrow Edit

--- resources/views/livewire/people/worktimes/index.blade.php
+++ resources/views/livewire/people/worktimes/index.blade.php
@@ -7,8 +7,8 @@
             placeholder="بحث بالتاريخ">
     </div> --}}
     <h1>دوامات {{ $person->name }}</h1>
-    <h2>مجموع الدوامات = {{ $personWorkTimesCount }}</h2>
-    <h2>مجموع الغياب = {{ $personUnAttendTimesCount }}</h2>
+    <h2>مجموع الدوامات = {{ number_format($personWorkTimesCount) }}</h2>
+    <h2>مجموع الغياب = {{ number_format($personUnAttendTimesCount) }}</h2>
     <a href="{{ route('people.index') }}" class="btn btn-dark text-white offset-4 mar-5">عرض جميع الأفراد</a>
     <a href="{{ route('people.worktimes.create', $person->id) }}" class="btn btn-dark text-white mar-5">إضافة
         دوام</a>
